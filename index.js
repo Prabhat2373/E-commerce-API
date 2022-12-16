a69e9cb89eed5a3b5edf9de9a388e1c60a59bf5e
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2022-12-16 20:43:28 +0530

    CHANGEEE

--- index.js
+++ index.js
@@ -28,7 +28,17 @@ mongoose
     });
 console.log(process.env.NODE_ENV);
 app.use(cookieParser());
+
 app.use(cors({ origin: process.env.NODE_ENV === 'production' ? 'https://e-commerce-web-opal.vercel.app' : 'http://localhost:3000', credentials: true, exposedHeaders: ['Set-Cookie', 'Date', 'ETag'] }))
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json;charset=UTF-8')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/user", UserRoutes)
