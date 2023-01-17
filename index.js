5ad9fec549047ab5aa91c9d2ce3a25594e00f235
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-01-17 16:04:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
     )
-    res.header('sameSite', 'none')
+    res.header('SameSite', 'None')
     next()
 })
 app.use(express.urlencoded({ extended: true }));
